654afe4f5533de36c9362c4db8f506f910e9a8f8
AsatovDias <d_assat@kbtu.kz>
2025-07-11 14:00:06 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+
+
+const PORT = 5000;
+
+const app =  express();
+app.get("/", (req, res)=>{
+    res.status(200).json("Server is working");
+});
+
+app.listen(PORT, ()=> {
+    console.log("Server working on port" + PORT);
+});
